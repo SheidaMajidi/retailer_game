2070758f5133b9e740d0c91510f580127b475882
Sheida Majidi <97121417+SheidaMajidi@users.noreply.github.com>
2024-05-20 19:16:40 -0400

    specific_dataset added

--- game_scraping/scraping.py
+++ game_scraping/scraping.py
@@ -3,20 +3,6 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 
-#  webdriver
-#def init_driver():
-#    # ChromeDriverManager to manage the ChromeDriver installation
-#    service = Service(ChromeDriverManager().install())
-#    driver = webdriver.Chrome(service=service)
-#    return driver
-
-#def to_excel(df, name):
-#    with pd.ExcelWriter(name + '.xlsx') as writer:
-#        df.to_excel(writer, 'Sheet1')
-#
-#driver = init_driver()
-
-
 
 def init_driver():
     try:
